416020af40eedba7cae0851be0dcf5e403d52625
LARS BERG <larsgbr@gmail.com>
2017-11-27 13:44:23 -0600

    added in elevation functionality

--- map1.py
+++ map1.py
@@ -6,13 +6,15 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt,ln in zip(lat,lon):
-	fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat,lon,elev):
+	print(type(el))
+	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
