0be3795769cd14a05c8274afdce2b3604f09707e
LARS BERG <larsgbr@gmail.com>
2017-11-27 14:55:38 -0600

    added in polygon functionality

--- map1.py
+++ map1.py
@@ -8,13 +8,23 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln, el in zip(lat,lon,elev):
-	print(type(el))
-	fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), fill=True,  color = 'grey', fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+	style_function=lambda x: {'fillColor':'green'}))
 
 map.add_child(fg)
 
