35888834e321bafa5f88c4a17ba679a903944df1
LARS BERG <larsgbr@gmail.com>
2017-11-27 10:39:57 -0600

    added in functionality to include locations from data file

--- map1.py
+++ map1.py
@@ -1,10 +1,18 @@
+# Folium is a Python Library that enables you to create different types of Leaflet maps
 import folium
+# Pandas is a Python package providing fast, flexible, and expressive data structures designed to make working with “relational” or “labeled” data both easy and intuitive.
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Mapbox Bright")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-	fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a Marker", icon=folium.Icon(color='green')))
+for lt,ln in zip(lat,lon):
+	fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a Marker", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
